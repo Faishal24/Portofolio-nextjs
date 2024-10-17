7f5947becf25a4470f323d2df64350e37aefc79a
Faishal <ishal.2401@gmail.com>
2024-10-17 09:36:17 +0700

    Change class to className

--- components/contents/Certificate.js
+++ components/contents/Certificate.js
@@ -6,10 +6,10 @@ import Image from "next/image";
 const Certificate = ({ src, alt, link }) => {
   const [isLoading, setIsLoading] = useState(true);
   return (
-    <div class="w-full">
+    <div className="w-full">
       {isLoading && (
-        <div class="animate-pulse">
-          <div class="bg-gray-400 dark:bg-gray-200 border border-gray-900 dark:border-none h-36 w-full rounded-md"></div>
+        <div className="animate-pulse">
+          <div className="bg-gray-400 dark:bg-gray-200 border border-gray-900 dark:border-none h-36 w-full rounded-md"></div>
         </div>
       )}
 
@@ -17,7 +17,7 @@ const Certificate = ({ src, alt, link }) => {
         <Image
           src={src}
           alt={alt}
-          class={`border border-gray-900 dark:border-none object-cover h-full w-full rounded-md transition transform hover:scale-105 ${
+          className={`border border-gray-900 dark:border-none object-cover h-full w-full rounded-md transition transform hover:scale-105 ${
             isLoading ? "hidden" : "block"
           }`}
           onLoad={() => setIsLoading(false)}
