243593cd337b3a712ed781dbdffc590a50f694d2
Faishal <ishal.2401@gmail.com>
2024-10-17 09:45:14 +0700

    Fix bug

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-WKDWF8GV"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{display:none, visibility:hidden}}
           ></iframe>
         </noscript>
 
