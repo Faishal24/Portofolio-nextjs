1c08b8af1be7a443fd4ab7d8427492e6f07b1725
Faishal <ishal.2401@gmail.com>
2024-10-19 19:35:56 +0700

    Move metadata code

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,7 @@
 import "./globals.css";
 import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { GoogleTagManager } from "@next/third-parties/google";
-import { baseUrl } from "./sitemap";
 
-export const metadata = {
-  metadataBase: new URL(baseUrl),
-  title: {
-    default: "Faishal's Portofolio",
-    template: "%s | Faishal's Portofolio",
-  },
-  description: "Created using Next.js and Tailwind CSS",
-  openGraph: {
-    title: "Faishal's Portofolio",
-    description: "Created using Next.js and Tailwind CS.",
-    url: baseUrl,
-    siteName: "My Portfolio",
-    locale: "id_ID",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
-    },
-  },
-  verification: { google: "UZumOKjgwFZuN5L6Y0f8urkUI_PzwlOBJm7eb2rD-tk" },
-};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- app/page.js
+++ app/page.js
@@ -4,6 +4,36 @@ import Banner from "@/components/Banner";
 import Hero from "@/components/Hero";
 import Body from "@/components/Body";
 import Footer from "@/components/Footer";
+import { baseUrl } from "./sitemap";
+
+export const metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Faishal's Portofolio",
+    template: "%s | Faishal's Portofolio",
+  },
+  description: "Created using Next.js and Tailwind CSS",
+  openGraph: {
+    title: "Faishal's Portofolio",
+    description: "Created using Next.js and Tailwind CS.",
+    url: baseUrl,
+    siteName: "My Portfolio",
+    locale: "id_ID",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  verification: { google: "UZumOKjgwFZuN5L6Y0f8urkUI_PzwlOBJm7eb2rD-tk" },
+};
 
 export default function Home() {
   return (
