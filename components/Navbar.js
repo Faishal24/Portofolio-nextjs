fa18336ed5e16f3c34e7cdf36c125e489a468650
Faishal <ishal.2401@gmail.com>
2024-10-19 13:05:55 +0700

    Add theme icon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useContext } from "react";
+import { IoMoon, IoSunny } from "react-icons/io5";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { ThemeContext } from "@/contexts/ThemeProvider";
 
@@ -8,7 +10,10 @@ const Navbar = () => {
   const { theme, changeTheme } = useContext(ThemeContext);
 
   return (
-    <nav role="navigation" className="flex justify-between h-14 sm:h-16 items-center w-screen px-5 sm:px-24 z-10 fixed backdrop-blur bg-gray-200/80 dark:bg-gray-900/80">
+    <nav
+      role="navigation"
+      className="flex justify-between h-14 sm:h-16 items-center w-screen px-5 sm:px-24 z-10 fixed backdrop-blur bg-gray-200/80 dark:bg-gray-900/80"
+    >
       <a
         href="https://24-remake.vercel.app/"
         className="text-xl text-gray-200 font-bold font-mono"
@@ -31,12 +36,27 @@ const Navbar = () => {
           />
         )}
       </a>
-      <p
-        className="text-xl text-gray-900 dark:text-gray-200 font-bold font-mono select-none"
-        onClick={changeTheme}
+
+      <motion.div
+        key={theme}
+        initial={{ opacity: 0, rotate: 180 }}
+        animate={{ opacity: 1, rotate: 360 }}
+        exit={{ opacity: 0, rotate: 0 }}
+        transition={{ duration: 0.5 }}
+        className="cursor-pointer"
       >
-        24
-      </p>
+        {theme == "dark" ? (
+          <IoMoon
+            className="text-2xl text-gray-200 font-bold font-mono select-none"
+            onClick={changeTheme}
+          />
+        ) : (
+          <IoSunny
+            className="text-2xl text-gray-900 font-bold font-mono select-none"
+            onClick={changeTheme}
+          />
+        )}
+      </motion.div>
     </nav>
   );
 };
